3f57325eca4cc48bed6f07165c2da75c18d85ae7
Bailey <111161049+byoung22@users.noreply.github.com>
2023-10-04 17:47:47 -0700

    Fetch pokeapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,50 @@
-import { useState, useEffect } from 'react'
-
-const data = {};
+import React, { useState, useEffect } from 'react'
 
 function generateId() {
   const id = Math.floor(Math.random() * 1293)
   return (id > 1017) ? id + 8983: id
 }
 
-// Fill data
-(() => {
-  for (let i = 0; i < 11; i++) {
-    const obj = {
-      id:
-    }
-  }
-})();
-
-function Card() {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  useEffect(() => {
-    const id = generateId();
-    async function fetchPokemon() {
-      try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const pokeData = await response.json();
-        setImageUrl(pokeData.sprites.front_default);
-      } catch (error) {
-        console.error("There was a problem with the fetch operation:", error);
-      }
-    }
-    fetchPokemon();
-  },[]);
-
-  
+function Card() {  
   return (
     <>
-      <img src={imageUrl}/>
+      <img/>
     </>
   );
 }
 
 function App() {
+  const [data, setDate] = useState([]);
+  // Fill data once
+  useEffect(() => {
+    async function fetchPokemon() {
+      for (let i = 0; i < 2; i++) {
+        const id = generateId();
+        try {
+          const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const pokeData = await response.json(); // Await the promise
+          const obj = {
+            id: id,
+            pokeData: pokeData,
+          }
+          const copy = [...data]
+          copy.push(obj);
+          setDate(copy)
+          console.log(data); // Log the new data
+        } catch (error) {
+          console.error("There was a problem with the fetch operation:", error);
+        }
+      }
+    }
+    fetchPokemon(); // Call the async function
+  }, []);
+
   return (
     <>
-    <Card/>
+      <Card/>
     </>
   )
 }
