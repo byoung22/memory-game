5437d7e8b1029fe50cd627526115d9c65b09688b
Bailey <111161049+byoung22@users.noreply.github.com>
2023-10-04 19:47:43 -0700

    Changed data to object

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,61 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 function generateId() {
   const id = Math.floor(Math.random() * 1293)
   return (id > 1017) ? id + 8983: id
 }
 
-function Card() {  
+function Card({ name, imageUrl, dataKey }) {
   return (
     <>
-      <img/>
+      <button data-key={dataKey}>
+      <img src={imageUrl} alt={name}/>
+      <p>{name}</p>
+      </button>
     </>
-  );
+  )
+}
+function CardList({ data }) {  
+  return Object.keys(data).map((key) => {
+    return (
+      <div key={key}>
+        {(data[key]) ? <Card name={data[key].name} imageUrl={data[key].imageUrl} dataKey={key}/> : <p>Loading...</p>}
+      </div>
+    );
+  });
 }
 
 function App() {
-  const [data, setDate] = useState([]);
-  // Fill data once
+  const [data, setDate] = useState({});
+  // Fetch data from pokeAPI
   useEffect(() => {
     async function fetchPokemon() {
+      const fetchedData = {};
       for (let i = 0; i < 2; i++) {
-        const id = generateId();
+        let id = generateId();
+        while (fetchedData[id]) id = generateId();
         try {
           const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
           const pokeData = await response.json(); // Await the promise
-          const obj = {
-            id: id,
-            pokeData: pokeData,
+          fetchedData[id] = {
+            name: pokeData.species.name,
+            imageUrl: pokeData.sprites.front_default
           }
-          const copy = [...data]
-          copy.push(obj);
-          setDate(copy)
-          console.log(data); // Log the new data
         } catch (error) {
           console.error("There was a problem with the fetch operation:", error);
         }
       }
+      setDate(fetchedData);
     }
     fetchPokemon(); // Call the async function
   }, []);
 
   return (
     <>
-      <Card/>
+      <CardList data={data}/>
     </>
   )
 }
